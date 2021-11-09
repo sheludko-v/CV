166621496bca16c2722e6e86f260e493892869b1
stigmat <stigmat.svo@gmail.com>
2021-11-09 14:08:35 +0300

    recaptcha

--- js/script.js
+++ js/script.js
@@ -1,39 +1,40 @@
 $(function (f) {
-    let element = f(".my-up");
-    f(window).scroll(function () {
-        element["fade" + (f(this).scrollTop() > 550 ? "In" : "Out")](500);
-    });
+  let element = f(".my-up");
+  f(window).scroll(function () {
+    element["fade" + (f(this).scrollTop() > 550 ? "In" : "Out")](500);
+  });
 });
 
+$(document).ready(function () {
+  $("#contact-form").on("submit", function (e) {
+    $.ajax({
+      type: "POST",
+      url: "/php/send.php",
+      data: $(this).serialize(),
+      success: function () {
+        swal("Класс!", "Ваше письмо отправлено!", "success");
+        $("#exampleModal").modal("hide");
+      },
+      error: function () {
+        swal("Упс...", "Письмо не отправлено :(", "error");
+        $("#exampleModal").modal("hide");
+      },
+    });
+    e.preventDefault();
+  });
+});
 
-// $(document).ready(function(){
-//     $('#contact-form').on('submit',function(e) { 
-//       let name = $("#name").val();
-//       let email = $("#email").val();
-//       let tel = $("#tel").val();
-//       let msg = $("#msg").val();
-//       $.ajax({
-//         type: "POST",
-//         url: "/php/send.php",
-//         data: {
-//           name: name,
-//           email: email,
-//           tel: tel,
-//           msg: msg,
-//           captcha: grecaptcha.getResponse()
-//         },
-//         success:function(){
-//           swal("Класс!", "Ваше письмо отправлено!", "success");
-//           $('#exampleModal').modal('hide');
-//         },
-//         error:function(){
-//           swal("Упс...", "Письмо не отправлено :(", "error");
-//           $('#exampleModal').modal('hide');
-//         }
-//       });
-//       e.preventDefault(); 
-//     });
-//   });
-
-
-  
+function get_action(form) 
+{
+    var v = grecaptcha.getResponse();
+    if(v.length == 0)
+    {
+        document.getElementById('captcha').innerHTML="You can't leave Captcha Code empty";
+        return false;
+    }
+    else
+    {
+        document.getElementById('captcha').innerHTML="Captcha completed";
+        return true; 
+    }
+}
