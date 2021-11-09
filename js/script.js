c19c8649363a144061da8dfbed96f12b9197cdbe
stigmat <stigmat.svo@gmail.com>
2021-11-09 16:58:10 +0300

    ajax & sweetalert

--- js/script.js
+++ js/script.js
@@ -7,34 +7,29 @@ $(function (f) {
 
 $(document).ready(function () {
   $("#contact-form").on("submit", function (e) {
-    $.ajax({
+    let response = grecaptcha.getResponse();
+
+    if(response.length == 0) {
+      swal("Упс...", "Вы не прошли проверку на робота...", "error");
+      return false;
+    } else {
+       $.ajax({
       type: "POST",
       url: "/php/send.php",
       data: $(this).serialize(),
       success: function () {
-        swal("Класс!", "Ваше письмо отправлено!", "success");
+        swal("Класс!", "Ваше письмо отправлено!", "success").then( function() { location.reload(); });
         $("#exampleModal").modal("hide");
       },
       error: function () {
-        swal("Упс...", "Письмо не отправлено :(", "error");
+        swal("Упс...", "Письмо не отправлено :(", "error").then( function() { location.reload(); });
         $("#exampleModal").modal("hide");
       },
     });
+    }
     e.preventDefault();
   });
 });
 
-function get_action(form) 
-{
-    var v = grecaptcha.getResponse();
-    if(v.length == 0)
-    {
-        document.getElementById('captcha').innerHTML="You can't leave Captcha Code empty";
-        return false;
-    }
-    else
-    {
-        document.getElementById('captcha').innerHTML="Captcha completed";
-        return true; 
-    }
-}
+
+    
