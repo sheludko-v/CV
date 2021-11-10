6d6c10230d46f351539a5ca844806e63e45d3ca5
stigmat <stigmat.svo@gmail.com>
2021-11-10 13:42:36 +0300

    animate 'mail me'

--- js/script.js
+++ js/script.js
@@ -9,7 +9,7 @@ $(document).ready(function () {
   $("#contact-form").on("submit", function (e) {
     let response = grecaptcha.getResponse();
 
-    if(response.length == 0) {
+    if (response.length == 0) {
       swal("Упс...", "Вы не прошли проверку на робота...", "error");
       return false;
     } else {
@@ -23,7 +23,9 @@ $(document).ready(function () {
           grecaptcha.reset();
         },
         error: function () {
-          swal("Упс...", "Письмо не отправлено :(", "error").then( function() { location.reload(); });
+          swal("Упс...", "Письмо не отправлено :(", "error").then(function () {
+            location.reload();
+          });
           $("form").trigger("reset");
           grecaptcha.reset();
         },
@@ -34,5 +36,35 @@ $(document).ready(function () {
   });
 });
 
+let pauseFactor = 15;
+function jiggleit(num) {
+  this.el = jiggleit.el("jiggle" + num);
+  this.to = 80;
+  this.jig = function () {
+    let c = this;
+    if (pauseFactor) {
+      c.t = c.t ? ++c.t : 1;
+      if (c.t % pauseFactor == 0) {
+        c.to = pauseFactor * 250;
+        c.t = 0;
+      } else c.to = 80;
+    }
+    c.el.style.left = parseInt(c.el.style.left) == 1 ? "-2px" : "1px";
+    setTimeout(function () {
+      c.jig();
+    }, c.to);
+  };
+  this.jig();
+}
 
-    
+jiggleit.el = function (id) {
+  return document.all ? document.all[id] : document.getElementById(id);
+};
+
+jiggleit.init = function () {
+  let i = 0;
+  while (jiggleit.el("jiggle" + i)) i++;
+  if (i--) for (i; i > -1; --i) new jiggleit(i);
+};
+
+if (document.all || document.getElementById) window.onload = jiggleit.init;
